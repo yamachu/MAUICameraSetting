f349717bcd0aff3d42ab85af2322a6fff017e29d
yamachu <yamachu.dev@gmail.com>
2021-06-13 17:21:57 +0900

    namespaceが変わってたので追従

--- MAUICameraSetting/Startup.cs
+++ MAUICameraSetting/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
 using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
 using MAUICameraSetting.Data;
 using MAUICameraSetting.Shared.Services;
 
